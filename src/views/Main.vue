baea06ee4830570d61d483d455d7e86080f63acb
Romario Torres <cs360.x@hotmail.com>
2023-02-21 16:34:02 -0600

    Función para guardar formulario con foto en b64

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -5,85 +5,87 @@
         </div>
         <div class="formulario">
             <b-container fluid="">
-                <b-row>
-                    <b-col sm="12" class="mt-3">
-                        <h3>
-                            Formulario
-                        </h3>
-                    </b-col>
-                    
-                    <b-col sm="4" class="mt-3">
-                        <label for="">País</label>
-                        <select  class="form-control form-control-sm" v-model="pais">
-                            <option value="">Selecciona</option>
-                            <option value="Argentina">Argentina</option>
-                            <option value="Bolivia">Bolivia</option>
-                            <option value="Brasil">Brasil</option>
-                            <option value="Colombia">Colombia</option>
-                            <option value="Costa Rica">Costa Rica</option>
-                            <option value="Chile">Chile</option>
-                            <option value="Ecuador">Ecuador</option>
-                            <option value="el salvador">El Salvador</option>
-                            <option value="España">España</option>
-                            <option value="usa">Estados Unidos</option>
-                            <option value="guatemala">Guatemala</option>
-                            <option value="honduras">Honduras</option>
-                            <option value="Inglaterra">Inglaterra</option>
-                            <option value="Italia">Italia</option>
-                            <option value="mexico">México</option>
-                            <option value="Nicaragua">Nicaragua</option>
-                            <option value="Panamá">Panamá</option>
-                            <option value="Perú">Perú</option>
-                            <option value="Uruguay">Uruguay</option>
-                            <option value="Suecia">Suecia</option>
-                            <option value="Suiza">Suiza</option>
-                            <option value="otro">Otro</option>
-                        </select>
-                    </b-col>
-                    <b-col sm="8" class="mt-3">
-                        <label for="">Ciudad / Departamento</label>
-                        <b-form-input type="text" size="sm" v-model="ciudad"></b-form-input>
-                    </b-col>
-                    <b-col sm="12" class="mt-3">
-                        <label for="">Región / Cono / </label>
-                        <b-form-input type="text" size="sm"  v-model="region"></b-form-input>
-                    </b-col>
-                    <b-col sm="12" class="mt-3">
-                        <label for="">Cargo</label>
-                        <select  class="form-control form-control-sm" v-model="cargo">
-                            <option value="">Seleccionar</option>
-                            <option value="pastor">Pastor</option>
-                            <option value="obrero">Obrero</option>
-                        </select>
-                    </b-col>
-                    <b-col sm="6" class="mt-3">
-                        <label for="">Nombres</label>
-                        <b-form-input type="text" size="sm" v-model="nombres"></b-form-input>
-                    </b-col>
-                    <b-col sm="6" class="mt-3">
-                        <label for="">Apellidos</label>
-                        <b-form-input type="text" size="sm"  v-model="apellidos"></b-form-input>
-                    </b-col>
-                    <b-col v-if="foto_cargada == false" sm="12" class="mt-3">
-                        <label for="">Foto</label>
-                            <div class="area-foto"  @dragenter.prevent.stop="dragStart" @dragleave="dragEnd" @dragover.prevent @drop="dragFile">
-                                Arrastra la foto aquí
+                <form @submit.prevent="guardar">
+                    <b-row>
+                        <b-col sm="12" class="mt-3">
+                            <h3>
+                                Formulario
+                            </h3>
+                        </b-col>
+                        
+                        <b-col sm="4" class="mt-3">
+                            <label for="">País</label>
+                            <select  class="form-control form-control-sm" required v-model="pais">
+                                <option value="">Selecciona</option>
+                                <option value="Argentina">Argentina</option>
+                                <option value="Bolivia">Bolivia</option>
+                                <option value="Brasil">Brasil</option>
+                                <option value="Colombia">Colombia</option>
+                                <option value="Costa Rica">Costa Rica</option>
+                                <option value="Chile">Chile</option>
+                                <option value="Ecuador">Ecuador</option>
+                                <option value="el salvador">El Salvador</option>
+                                <option value="España">España</option>
+                                <option value="usa">Estados Unidos</option>
+                                <option value="guatemala">Guatemala</option>
+                                <option value="honduras">Honduras</option>
+                                <option value="Inglaterra">Inglaterra</option>
+                                <option value="Italia">Italia</option>
+                                <option value="mexico">México</option>
+                                <option value="Nicaragua">Nicaragua</option>
+                                <option value="Panamá">Panamá</option>
+                                <option value="Perú">Perú</option>
+                                <option value="Uruguay">Uruguay</option>
+                                <option value="Suecia">Suecia</option>
+                                <option value="Suiza">Suiza</option>
+                                <option value="otro">Otro</option>
+                            </select>
+                        </b-col>
+                        <b-col sm="8" class="mt-3">
+                            <label for="">Ciudad / Departamento</label>
+                            <b-form-input type="text" size="sm" required v-model="ciudad"></b-form-input>
+                        </b-col>
+                        <b-col sm="12" class="mt-3">
+                            <label for="">Región / Cono / </label>
+                            <b-form-input type="text" size="sm"  required v-model="region"></b-form-input>
+                        </b-col>
+                        <b-col sm="12" class="mt-3">
+                            <label for="">Cargo</label>
+                            <select  class="form-control form-control-sm" required v-model="cargo">
+                                <option value="">Seleccionar</option>
+                                <option value="pastor">Pastor</option>
+                                <option value="obrero">Obrero</option>
+                            </select>
+                        </b-col>
+                        <b-col sm="6" class="mt-3">
+                            <label for="">Nombres</label>
+                            <b-form-input type="text" id="nombre_form" size="sm" required v-model="nombres"></b-form-input>
+                        </b-col>
+                        <b-col sm="6" class="mt-3">
+                            <label for="">Apellidos</label>
+                            <b-form-input type="text" size="sm"  required v-model="apellidos"></b-form-input>
+                        </b-col>
+                        <b-col v-if="foto_cargada == false" sm="12" class="mt-3">
+                            <label for="">Foto</label>
+                                <div class="area-foto"  @dragenter.prevent.stop="dragStart" @dragleave="dragEnd" @dragover.prevent @drop="dragFile">
+                                    Arrastra la foto aquí
+                                </div>
+                        </b-col>
+                        <b-col v-if="foto_cargada" sm="6" class="mt-3">
+                            <div class="foto_vista_previa">
+                                <img :src="fb64" alt="" class="img_vista_previa">
                             </div>
-                    </b-col>
-                    <b-col v-if="foto_cargada" sm="6" class="mt-3">
-                        <div class="foto_vista_previa">
-                            <img :src="fb64" alt="" class="img_vista_previa">
-                        </div>
-                    </b-col>
-                    <b-col v-if="foto_cargada" sm="6" class="mt-3">
-                        <div class="contenedor_botonera">
-                            <b-button type="button" variant="danger" size="sm" @click="cambiar_foto">Cambiar</b-button>
-                        </div>
-                    </b-col>
-                    <b-col sm="12" class="mt-3 d-flex flex-row-reverse">
-                        <b-button type="submit" variant="outline-success" size="sm">Guardar</b-button>
-                    </b-col>
-                </b-row>
+                        </b-col>
+                        <b-col v-if="foto_cargada" sm="6" class="mt-3">
+                            <div class="contenedor_botonera">
+                                <b-button type="button" variant="danger" size="sm" @click="cambiar_foto">Cambiar</b-button>
+                            </div>
+                        </b-col>
+                        <b-col v-if="fb64 != ''" sm="12" class="mt-3 d-flex flex-row-reverse">
+                            <b-button type="submit" variant="outline-success" size="sm">Guardar</b-button>
+                        </b-col>
+                    </b-row>
+                </form>
             </b-container>
         </div>
     </div>
@@ -92,6 +94,9 @@
 <script>
 
 import { ipcRenderer } from 'electron'
+import axios from 'axios'
+import moment from 'moment'
+import { minix } from '../components/functions/alertas'
 
 export default {
     name: 'Main',
@@ -109,7 +114,6 @@ export default {
     },
     methods: {
         async dragFile(e){
-            console.log(e.dataTransfer.files)
 
             let ruta = e.dataTransfer.files[0].path
             let r = await ipcRenderer.invoke('fotob64', ruta)
@@ -127,6 +131,40 @@ export default {
         cambiar_foto(){
             this.foto_cargada = false
             this.fb64 = ''
+        },
+        async guardar(){
+            let f = {
+                data: {
+                    nombres: this.nombres.toUpperCase().trim(),
+                    apellidos: this.apellidos.toUpperCase().trim(),
+                    pais: this.pais.toUpperCase(),
+                    ciudad: this.ciudad.toUpperCase().trim(),
+                    region: this.region.trim(),
+                    cargo: this.cargo.toUpperCase(),
+                    retiro_year: moment(Date.now()).format('YYYY'),
+                    foto_b64: this.fb64,
+                    token_sesion: 'wwwwww',
+                    creacion: moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')
+                }
+            }
+
+            let r = await axios.post('http://localhost:1337/api/registros', f)
+
+            if (r.status == 200) {
+                minix({icon: 'success', mensaje: 'GUARDADO CON EXITO', tiempo: 3000})
+
+                this.nombres = ''
+                this.apellidos = ''
+                this.foto_cargada = false
+                this.fb64 = ''
+
+                document.getElementById('nombre_form').focus()
+
+            }else{
+
+                minix({icon: 'error', mensaje: 'ALGO SALIÓ MAL', tiempo: 3000})
+            }
+
         }
     },
 }
