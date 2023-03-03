cd70f2d49c463242d260fc1a54edf02cd8ef1811
Romario Torres <cs360.x@hotmail.com>
2023-03-03 15:41:43 -0600

    Se agrega ip publica

--- src/components/config/parametros.js
+++ src/components/config/parametros.js
@@ -1,7 +1,7 @@
 module.exports = {
     TE: '10h',
-    IP: '172.16.10.195',
-    PUBLIC_IP: '172.16.10.195',
+    IP: process.env.NODE_ENV == 'development' ? 'localhost' : 'servidorpm.ddns.net',
+    PUBLIC_IP: 'servidorpm.ddns.net',
     PUERTO: process.env.NODE_ENV == 'development' ? 5555 : 7171,
     //PUERTO: 7171, 
     SECRET_TOKEN: 'c_:pwNc4;nCz#!wS4YdkeSB}%CFz[kf-giGR)}9RKpF2yX[w.dTJ2EPUtvDV()cG:fa:R]?5,VS#A}Cu',
